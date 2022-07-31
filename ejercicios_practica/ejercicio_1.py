c49ceb6fde2344d54fc29b658e48c8ffe727a022
francisco-iriarte <iriartef@gmail.com>
2022-07-31 19:43:02 -0300

    Update ejercicio_1.py
    Ejercicio_1 finalizado

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -18,6 +18,9 @@ numero_2 = int(input('Ingrese el segundo número:\n'))
 
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
+print ("             ")
+print ("Ejercicio 1-1")
+print ("-------------")
 if numero_1 > numero_2:
     print ("numero_1 = ", numero_1, " es mayor que numero_2 = ", numero_2)
 elif numero_1 < numero_2:
@@ -28,6 +31,9 @@ else :
 
 # Verifique si el numero_1 positivo, negativo o cero
 # Imprima el resultado en cada caso
+print ("             ")
+print ("Ejercicio 1-2")
+print ("-------------")
 if numero_1 > 0:
     print ("numero_1 = ", numero_1, " es positivo")
 elif numero_1 < 0:
@@ -38,12 +44,23 @@ else :
 
 # Verifique si el numero_1 es mayor a 0 y menor a 100
 # Imprima en pantalla si se cumple o no la condición
-
+#
+print ("             ")
+print ("Ejercicio 1-3")
+print ("-------------")
 if numero_1 > 0 and numero_1 < 100 :
-    print ("numero_1 = ", numero_1, ", SE cumple la condición. Numero_1 es mayor que 0 y menor que 100")
+    print ("numero_1 = ", numero_1, " SE cumple la condición. Numero_1 es mayor que 0 y menor que 100")
 else : 
-    print ("numero_1 = ", numero_1, ", NO SE cumple la condición. Numero_1 NO ES mayor que 0 NI menor que 100")
-    
+    print ("numero_1 = ", numero_1, " NO SE cumple la condición. Numero_1 NO ES mayor que 0 NI menor que 100")
+#
 # Verifique si el numero_1 es menor a 10 o el numero_2
 # es mayor a -2
 # Imprima en pantalla si se cumple o no la condición
+#
+print ("             ")
+print ("Ejercicio 1-4")
+print ("-------------")
+if numero_1 < 10 or numero_2 > -2 :
+    print ("numero_1 = ", numero_1, "numero_2 = ", numero_2, "SE cumple la condición. Numero_1 es menor 10 o numero_2, mayor que -2")
+else : 
+    print ("numero_1 = ", numero_1, "numero_2 = ", numero_2, "NO SE cumple la condición. Numero_1 NO ES menor 10 o  numero_2, NO ES mayor que -2")
