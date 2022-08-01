1d109469ecd89aefdddf951c93b2f021afe06bb0
francisco-iriarte <iriartef@gmail.com>
2022-07-31 22:24:58 -0300

    Update ejercicio_4.py
    cicio completado

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -8,14 +8,39 @@
 # que aparecen en verde con el hashtag "#"
 
 # Ejemplos variables de texto
+from turtle import clearscreen
 
-texto_1 = '5'
-texto_2 = '7'
 
+clearscreen
+numero_1 = 8
+numero_2 = 70
+texto_1 = str(numero_1)
+texto_2 = str(numero_2)
 # 1-Verifique cual de los dos textos es mayor alfabéticamente
 # La comparación alfabética es aquella que se logra cuando
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
+
+print ("             ")
+print ("Ejercicio 1-1")
+print ("-------------")
+print (" ")
+print (" ")
+if texto_1 > texto_2:
+    print('texto_1 = {} es mayor que texto=2 {}'.format(texto_1, texto_2))
+elif texto_1 < texto_2:
+    print('texto_1 = {} es menor que texto=2 {}'.format(texto_1, texto_2))
+else :
+    print('texto_1 = {} es igual a texto_2 = {}'.format(texto_1, texto_2))
+print (" ")
+print ("Otra forma")
+print (" ")
+if texto_1 > texto_2:
+    print ("texto_1 = ", texto_1, " es mayor que texto_2 =", texto_2)
+elif texto_1 < texto_2 :
+    print ("texto_1 = ", texto_1, " es menor que texto_2 =", texto_2)
+else :
+    print ("texto_1 = ", texto_1, " es igual a texto_2 =", texto_2)
 
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
@@ -24,6 +49,19 @@ texto_2 = '7'
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
 
+nuevo_numero_1 = int(texto_1)
+nuevo_numero_2 = int(texto_2)
+
+print ("             ")
+print ("Ejercicio 1-2")
+print ("-------------")
+if numero_1 > numero_2:
+    print ("nuevo_numero_1 = ", nuevo_numero_1, " es MAYOR que nuevo_numero_2 = ", nuevo_numero_2)
+elif numero_1 < numero_2:
+    print ("nuevo_numero_1 = ", nuevo_numero_1, " es MENOR que nuevo_numero_2 = ", nuevo_numero_2)
+else :
+    print ("nuevo_numero_1 = ", nuevo_numero_1, " es IGUAL que nuevo_numero_2 = ", nuevo_numero_2)
+#
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
 # Siendo números tiene sentido, pero son caracteres, texto,
@@ -32,3 +70,6 @@ texto_2 = '7'
 # Esta pregunta estará repetida en el Campus para que puedan
 # responder.
 # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
+# 
+# Según lo hallado en Google se explica por UNICODE
+#
